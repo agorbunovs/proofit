cbe963015c676b89fd7224846ee3eb0c1eb119ed
Anatolijs Gorbunovs <a@mathlogic.lv>
2020-10-30 14:09:53 +0200

    Added Policy test.

---
+++ src/test/java/lv/proofit/techtask/PolicyTest.java
@@ -0,0 +1,103 @@
+package lv.proofit.techtask;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+public class PolicyTest {
+
+	@Test
+	public void testConstructorOK() {
+		PolicySubObject subA = new PolicySubObject("Subobject A", new BigDecimal("100.0"), RiskType.FIRE);
+		PolicySubObject subB = new PolicySubObject("Subobject B", new BigDecimal("8.0"), RiskType.THEFT);
+		PolicySubObject subC = new PolicySubObject("Subobject C", new BigDecimal("500.0"), RiskType.FIRE);
+		PolicySubObject subD = new PolicySubObject("Subobject D", new BigDecimal("102.51"), RiskType.THEFT);
+		List<PolicyObject> objects = Arrays.asList(
+				new PolicyObject("Empty object", Collections.EMPTY_LIST),
+				new PolicyObject("Single element object", Arrays.asList(subA)),
+				new PolicyObject("Three element object", Arrays.asList(subB, subC, subD)));
+		Policy policy = new Policy(
+				"LV20-02-100000-5",
+				PolicyStatus.REGISTERED,
+				objects
+		);
+		Assert.isTrue(policy.getNumber().equals("LV20-02-100000-5"), "Policy number does not match.");
+		Assert.isTrue(policy.getStatus().equals(PolicyStatus.REGISTERED), "Policy status does not match.");
+		Assert.isTrue(policy.getObjects().containsAll(objects) && objects.containsAll(policy.getObjects()), "Policy objects do not match.");
+	}
+	
+	@Test
+	public void testConstructorThrowsNumberNull() {
+		PolicySubObject subA = new PolicySubObject("Subobject A", new BigDecimal("100.0"), RiskType.FIRE);
+		PolicySubObject subB = new PolicySubObject("Subobject B", new BigDecimal("8.0"), RiskType.THEFT);
+		PolicySubObject subC = new PolicySubObject("Subobject C", new BigDecimal("500.0"), RiskType.FIRE);
+		PolicySubObject subD = new PolicySubObject("Subobject D", new BigDecimal("102.51"), RiskType.THEFT);
+		List<PolicyObject> objects = Arrays.asList(
+				new PolicyObject("Empty object", Collections.EMPTY_LIST),
+				new PolicyObject("Single element object", Arrays.asList(subA)),
+				new PolicyObject("Three element object", Arrays.asList(subB, subC, subD)));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Policy(
+				null,
+				PolicyStatus.REGISTERED,
+				objects
+		));
+	}
+	
+	@Test
+	public void testConstructorThrowsStatusNull() {
+		PolicySubObject subA = new PolicySubObject("Subobject A", new BigDecimal("100.0"), RiskType.FIRE);
+		PolicySubObject subB = new PolicySubObject("Subobject B", new BigDecimal("8.0"), RiskType.THEFT);
+		PolicySubObject subC = new PolicySubObject("Subobject C", new BigDecimal("500.0"), RiskType.FIRE);
+		PolicySubObject subD = new PolicySubObject("Subobject D", new BigDecimal("102.51"), RiskType.THEFT);
+		List<PolicyObject> objects = Arrays.asList(
+				new PolicyObject("Empty object", Collections.EMPTY_LIST),
+				new PolicyObject("Single element object", Arrays.asList(subA)),
+				new PolicyObject("Three element object", Arrays.asList(subB, subC, subD)));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Policy(
+				"LV20-02-100000-5",
+				null,
+				objects
+		));
+	}
+	
+	@Test
+	public void testConstructorThrowsObjectsNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Policy(
+				"LV20-02-100000-5",
+				PolicyStatus.REGISTERED,
+				null
+		));
+	}
+	
+	@Test
+	public void testConstructorThrowsNumberBlank() {
+		PolicySubObject subA = new PolicySubObject("Subobject A", new BigDecimal("100.0"), RiskType.FIRE);
+		PolicySubObject subB = new PolicySubObject("Subobject B", new BigDecimal("8.0"), RiskType.THEFT);
+		PolicySubObject subC = new PolicySubObject("Subobject C", new BigDecimal("500.0"), RiskType.FIRE);
+		PolicySubObject subD = new PolicySubObject("Subobject D", new BigDecimal("102.51"), RiskType.THEFT);
+		List<PolicyObject> objects = Arrays.asList(
+				new PolicyObject("Empty object", Collections.EMPTY_LIST),
+				new PolicyObject("Single element object", Arrays.asList(subA)),
+				new PolicyObject("Three element object", Arrays.asList(subB, subC, subD)));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Policy(
+				" \t\n  ",
+				null,
+				objects
+		));
+	}
+	
+	@Test
+	public void testConstructorThrowsObjectsEmpty() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Policy(
+				"LV20-02-100000-5",
+				PolicyStatus.REGISTERED,
+				Collections.EMPTY_LIST
+		));
+	}
+}
